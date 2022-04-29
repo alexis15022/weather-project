6893d56613e557254885251e4cb6d62a31a8de1c
alexis15022 <104448060+alexis15022@users.noreply.github.com>
2022-04-29 07:56:44 +0200

    changed weather

---
+++ src/index.js
@@ -0,0 +1,50 @@
+let now = new Date();
+
+let dateTime = document.querySelector("#date-time");
+
+let hours = now.getHours();
+if (hours < 10) {
+  hours = `0${hours}`;
+}
+let minutes = now.getMinutes();
+if (minutes < 10) {
+  minutes = `0${minutes}`;
+}
+let dayIndex = now.getDay();
+let days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+
+dateTime.innerHTML = `${days[dayIndex]} ${hours}:${minutes}`;
+
+function displayWeatherCondition(response) {
+  document.querySelector("#city-name").innerHTML = response.data.name;
+  document.querySelector("#temperature").innerHTML = Math.round(
+    response.data.main.temp
+  );
+}
+
+function search(city) {
+  let apiKey = "4a535f86e8e321970f7dfc8efffaf2c8";
+  let units = "metric";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${units}`;
+  axios.get(apiUrl).then(displayWeatherCondition);
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+
+  let city = document.querySelector("#city-input").value;
+  search(city);
+}
+
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handleSubmit);
+
+search("New York");
