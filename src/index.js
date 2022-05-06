87db757004c75f388900aafc618b4848e4362fbf
alexis15022 <104448060+alexis15022@users.noreply.github.com>
2022-05-06 19:39:42 +0200

    changed layout added icon

--- src/index.js
+++ src/index.js
@@ -34,6 +34,12 @@ function displayWeatherCondition(response) {
   humidityElement.innerHTML = response.data.main.humidity;
   let windElement = document.querySelector("#wind");
   windElement.innerHTML = Math.round(response.data.wind.speed);
+  let iconElement = document.querySelector("#icon");
+  iconElement.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
+  iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 
 function search(city) {
